45216672807997467da6ac0222e87a35605864bf
IDEAriketak <correccion670@gmail.com>
2024-03-27 20:00:33 +0100

    Update 7 LIVE SD Stable Diffusion XL 1.py

--- pages/7 LIVE SD Stable Diffusion XL 1.py
+++ pages/7 LIVE SD Stable Diffusion XL 1.py
@@ -6,6 +6,7 @@ import urllib.request
 from PIL import Image
 import streamlit as st
 
+from transformers import AutoModel, AutoTokenizer
 from PIL import Image
 
 def local_css(file_name):
@@ -21,24 +22,20 @@ st.image("images/banner.jpg")
 
 # ---- LOAD
 local_css("styles/style.css")
-#openai.api_key = "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"
-
-#original it was working
-#openai.api_key = "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"
-
-gr.load("models/stabilityai/stable-diffusion-xl-base-1.0").launch('share=True')
-
-#OPENAI_API_KEY = "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"
-#openai.api_key = os.getenv(OPENAI_API_KEY)
-#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
-
 
 
 st.subheader('Stable Diffusion XL 1.0')
 st.caption("Testing.")
 # text input box for image recognition
 img_description = st.text_input('Enter image prompt. e.g. hyperrealistic, female web developer coding on a full moon night, back view, wide angle lens')
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model_name = "models/stabilityai/stable-diffusion-xl-base-1.0"
+    model = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return model, tokenizer
 
+model, tokenizer = load_model()
 #if st.button('Generate Image'):    
     #generated_img = generate_image(img_description)
     #st.image(generated_img)
