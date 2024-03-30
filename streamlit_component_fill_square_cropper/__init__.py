141882ccfacd5e2d7de699148f08a9a66660f206
IDEAriketak <correccion670@gmail.com>
2024-03-30 17:10:40 +0100

    updated

--- streamlit_component_fill_square_cropper/__init__.py
+++
@@ -1,5 +0,0 @@
-import streamlit.components.v1 as components
-component_zero = components.declare_component(
-    name='fill_square_cropper',
-    path='./streamlit_component_fill_square_cropper'
-)
