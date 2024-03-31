26960d0407f700b9879d053386315d2450b7e68c
IDEAriketak <correccion670@gmail.com>
2024-03-31 16:19:27 +0200

    Update 3 LIVE Square Filler_app.py

--- pages/3 LIVE Square Filler_app.py
+++ pages/3 LIVE Square Filler_app.py
@@ -7,7 +7,6 @@ import streamlit as st
 import numpy as np
 import requests
 from io import BytesIO
-from streamlit_component_fill_square_cropper import fill_square_cropper as imported_fill_square_cropper
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -22,6 +21,46 @@ st.image("images/banner.jpg")
 
 # ---- LOAD
 local_css("styles/style.css")
+
+def fill_square_cropper(img):
+    imgsz = [img.height, img.width]
+
+    original_size = imgsz
+
+    smallestsize = min(imgsz)
+    biggestsize = max(imgsz)
+
+    #get vertical color filler
+    avg_color_per_row = np.average(img, axis=0)
+    avg_color = np.average(avg_color_per_row, axis=0)
+
+    if img.height > img.width:
+      #height is bigger than width
+      area = (0, 0, img.width + (img.height - img.width),img.height)
+      cropped_img = img.crop(area)
+      newimgsz = [cropped_img.height, cropped_img.width]
+    
+      newimg = Image.new('RGB', ([newimgsz[1],newimgsz[0]]), (round(avg_color[0]), round(avg_color[1]), round(avg_color[2])))
+
+      newpos = (img.height-img.width)
+      newpos = newpos/2
+      newimg.paste(img,(int(newpos),0))
+
+      return newimg
+
+    #vertically add color
+    if img.width > img.height: 
+      area = (0, 0, img.width, img.height+ (img.width - img.height))
+      cropped_img = img.crop(area)
+      newimgsz = [cropped_img.height, cropped_img.width]
+    
+      newimg = Image.new('RGB', ([newimgsz[1],newimgsz[0]]), (round(avg_color[0]), round(avg_color[1]), round(avg_color[2])))
+ 
+      newpos = (img.width-img.height)
+      newpos = newpos/2
+      newimg.paste(img,(0,(int(newpos))))
+
+      return newimg
 
 # ---- TABS
 tab1, tab2 = st.tabs(["Demo","Application"])
@@ -40,14 +79,14 @@ with tab1:
     img = Image.open(BytesIO(response.content))
     img.load()
 
-    generated_img = imported_fill_square_cropper(img)
+    generated_img = fill_square_cropper(img)
     st.image(generated_img)
 
     response = requests.get(url2)
     img = Image.open(BytesIO(response.content))
     img.load()
 
-    generated_img = imported_fill_square_cropper(img)
+    generated_img = fill_square_cropper(img)
     st.image(generated_img)
    
 with tab2:
@@ -58,5 +97,5 @@ with tab2:
   if uploaded_file is not None: 
    img = Image.open(uploaded_file)
    img.load()
-   generated_img = imported_fill_square_cropper(img)
+   generated_img = fill_square_cropper(img)
    st.image(generated_img)
