9d7a52159a44dc008ac3604568dea5ddef6d2bf7
IDEAriketak <correccion670@gmail.com>
2024-03-31 20:25:22 +0200

    update

--- streamlit_component_fill_square_cropper101/streamlit_component_fill_square_cropper101/__init__.py
+++ streamlit_component_fill_square_cropper101/streamlit_component_fill_square_cropper101/__init__.py
@@ -25,7 +25,7 @@ if not _RELEASE:
     )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
+    build_dir = os.path.join(parent_dir, "streamlit_component_square_filler_cropper101/frontend/build")
     component = components.declare_component("streamlit_component_square_filler_cropper101", path=build_dir)
 
 
