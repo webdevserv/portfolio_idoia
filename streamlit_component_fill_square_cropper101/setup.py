e9198d8d20b340ae581acdc56e64cbe4a1cc861b
IDEAriketak <correccion670@gmail.com>
2024-03-31 19:46:25 +0200

    updated

--- streamlit_component_fill_square_cropper101/setup.py
+++ streamlit_component_fill_square_cropper101/setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     description="streamlit_component_fill_square_cropper, adds a filler when needed of the avg color pixel.",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/webdevserv/streamlit_component_fill_square_cropper",
+    url="https://github.com/webdevserv/streamlit_component_fill_square_cropper101",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
