5957481202da815504427fc774ca03a92f06067e
IDEAriketak <correccion670@gmail.com>
2024-04-04 15:21:41 +0200

    sidebar added

--- iCodeIdoia_portfolio.py
+++ iCodeIdoia_portfolio.py
@@ -26,7 +26,18 @@ with st.spinner("Loading..."):
 
 # ---- LOAD
 local_css("styles/style.css")
-
+#sidebar
+with st.sidebar:
+        st.image("images/modern.png")
+        st.markdown("---")
+        st.markdown(
+            'Created by <a href="https://webdevserv.github.io/html_bites/dev/webdev.html" target="_blank">webdevserv</a>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '<div style="margin: 0.75em 0;"><a href="https://www.buymeacoffee.com/Artgen" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            unsafe_allow_html=True,
+        )
 # ---- HEADER 
 with st.container():   
  st.write("Welcome to my iCode portfolio, I am a web developer with international experience (London and New York).")
@@ -60,7 +71,6 @@ with tab1:
          - Creating and styling web apps with Angular and Bootstrap.
          - WP child theme creation.
          """)
-         """button(username="artgen", floating=False, width=221)"""
 
 with tab2: 
     col1, col2 =  st.columns([2,3])
